f38d4a9423ce9c17c56ef57f7d146f5ad41175c8
KISHOREBM <154236719+KISHOREBM@users.noreply.github.com>
2024-10-01 00:00:53 +0530

    Delete empback directory

--- empback/Empdetail/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import create_emp,get_emp,user_login,sign_up
-urlpatterns=[
-    path("createmp/",create_emp),
-    path("getemp/",get_emp),
-    path("login/",user_login),
-    path("signup/",sign_up),
-]
