0909b0e80baf7e80cb09e3e33b7858021db8bf12
KISHOREBM <kishorkumar4735@gmail.com>
2025-04-25 02:32:14 +0530

    Added empfront folder contents to main repo

---
+++ empback/empfront/src/Components/Signup.jsx
@@ -0,0 +1,130 @@
+import axios from 'axios'
+import React, {useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+const Signup = () => {
+    const [username,setusername]=useState("")
+    const [email,setemail]=useState("")
+    const [password1,setpassword1]=useState("")
+    const [password2,setpassword2]=useState("")
+    const navigate=useNavigate()
+    const [first,setfirst]=useState()
+    const [third,setthird]=useState()
+    const [secand,setsecand]=useState()
+    const [forth,setforth]=useState()
+    const [enterotp, setenteropt] = useState(false)
+    const [otp,setotp]=useState(0)
+    const checkotp=(e)=>{
+      e.preventDefault();
+      let value = first*1000+secand*100+third*10+forth*1
+      // console.log(first*1000,secand*100,third*10,forth)
+      // console.log(value)
+      // console.log(otp)
+      setfirst('')
+      setsecand('')
+      setthird('')
+      setforth('')
+      if(value==otp)
+      {
+        toast("verified sucesssfully")
+        setenteropt(false)
+        submi()
+        
+        return
+      }
+      
+      toast("wrong otp")
+      return
+    }
+    const VerifyOtp=async(e)=>{
+      e.preventDefault();
+      if(password1!=password2)
+        {
+            toast.error("password dosen't match")
+            return;
+        }
+        setenteropt(true);
+        const response = await axios.post("http://127.0.0.1:8000/app/otpverify/",{'email':email})
+        if(response.data.detail)
+        {
+            toast("sucessfully created");
+            setotp(response.data.value);
+            
+        }
+        else{
+            toast("Some error occured");
+            return
+        }
+
+    }
+    const submi=async()=>
+    {
+        
+        const response = await axios.post("http://127.0.0.1:8000/app/signup/",{"username":username,"email":email,"password":password1,"adname":username})
+        console.log(response.data)
+        if(response.data.detail==="true")
+        {
+            navigate("/login")
+        }
+        else{
+            if (response.data['info']['username'])
+              
+              toast.error(`${response.data['info']['username']}`)
+            
+            if (response.data.info['non_field_errors'])
+              toast.error(`${response.data["info"]['non_field_errors']}`)
+
+        }
+    }
+    return (
+    <div className='flex justify-center items-center h-screen relative w-full sm:w-auto sm:ml-[10px] overflow-scroll' >
+      {!enterotp &&
+    <div className='flex border-2 border-black w-[400px] sm:w-auto rounded-[20px] items-center '>
+        <form onSubmit={VerifyOtp}>
+        <div className='flex flex-col m-2 p-2 space-y-2 w-full'>
+          <label htmlFor="">Name:</label>
+          <input type="text" placeholder='Enter Name' className='outline-none w-[300px]' required value={username} onChange={(e)=>{setusername(e.target.value)}} />
+        </div>
+        <div className='flex flex-col m-2 p-2 space-y-2 w-full'>
+          <label htmlFor="">Email:</label>
+          <input type="email" placeholder='Email' className='outline-none w-[300px]' required value={email} onChange={(e)=>{setemail(e.target.value)}}/>
+        </div>
+        <div className='flex flex-col m-2 p-2 space-y-2 w-full'>
+          <label htmlFor="">Password:</label>
+          <input type="password" placeholder='Password' className='outline-none w-[300px]' required value={password1} onChange={(e)=>{setpassword1(e.target.value)}}/>
+        </div>
+        <div className='flex flex-col m-2 p-2 space-y-2 w-full'>
+          <label htmlFor="">Confirm Password:</label>
+          <input type="password" placeholder='Confirm Password' className='outline-none w-[300px]' required value={password2} onChange={(e)=>{setpassword2(e.target.value)}}/>
+        </div>
+        <div className='flex flex-row m-2 p-2 justify-evenly w-full'>
+        <Link to="/login">Login?</Link>
+          <input type="submit"  className='outline-none w-[70px]  bg-[aqua] border-2 border-black rounded-[20px] text-white cursor-pointer' />
+        </div>
+
+      </form>
+    </div>
+      }
+    {enterotp && <form  className='flex h-screen  w-full justify-center items-center absolute bg-[rgba(0,0,0,0.5)]' onSubmit={checkotp}>
+        <div className='flex justify-center item-center flex-col  border-[3px] rounded-[19px] w-[400px] h-[350px] '>
+            <div className='flex m-[4px] p-[2px] w-full justify-center'>
+                <h1 className='font-serif text-[40px] '>Enter the otp</h1>
+            </div>
+            <div className='flex  justify-center p-[2px] '>
+                <input type="numeric" name="" id="" maxLength="1" value={first} onChange={(e)=>{setfirst(e.target.value)}} className=' m-[20px] w-[20px] border-2 rounded-[20px] text-center'/>
+                <input type="numeric" name="" id="" maxLength="1" value={secand} onChange={(e)=>{setsecand(e.target.value)}} className=' m-[20px] w-[20px] border-2 rounded-[20px] text-center'/>
+                <input type="numeric" name="" id="" maxLength="1" value={third} onChange={(e)=>{setthird(e.target.value)}} className=' m-[20px] w-[20px] border-2 rounded-[20px] text-center'/>
+                <input type="numeric" name="" id="" maxLength="1" value={forth} onChange={(e)=>{setforth(e.target.value)}} className=' m-[20px] w-[20px] border-2 rounded-[20px] text-center'/>
+            </div>
+            <div className='flex w-full justify-center items-center '>
+                <input type="submit" value="Verify" className=' cursor-pointer m-[5px] p-[10px] w-[100px]  justify-center items-center bg-green-500 text-white text-center rounded-[23px] ' />
+                <button className='cursor-pointer m-[5px] p-[10px] w-[80px] justify-center items-center bg-green-500 text-white text-center rounded-[23px] ' onClick={()=>{setenteropt(!enterotp)}}>back</button>
+            </div>
+        </div>
+    </form>}
+  </div>
+  
+  )
+}
+
+export default Signup
---
+++ empback/empfront/src/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import React, { useContext,useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Admininfo } from '../Context/Context'
+import {  toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import add from "../assets/add.png"
+import employeedetail from "../assets/employeedetail.png"
+import login from "../assets/login.png"
+import logout from "../assets/logout.png"
+import signupi from '../assets/signup.png';
+const Navbar = () => {
+    const {setIsAuth,setadname,adname,isAuth}=useContext(Admininfo);
+    const [showword,setshowword] = useState()
+    const navigate= useNavigate()
+    const change=()=>
+    {
+        if(isAuth)
+        {    
+            
+            console.log("in false");
+            console.log(isAuth)
+            toast.error("loged out")
+            setIsAuth(false)
+            setadname("")
+            localStorage.removeItem("isAuth")
+            localStorage.removeItem("adname")
+        }
+        navigate("/login");
+    }
+    useEffect(()=>{
+        if(window.innerWidth < 330)
+        {
+            setshowword(flase)
+        }
+        else{
+            setshowword(true)
+        }
+    },[window.screen])
+  return (
+    <div className={`flex flex-col  bg-[#4a0e4a] w-[200px]  text-white justify-between sm:w-[60px] h-screen overflow-scroll flex-shrink-0`}>
+        <div>
+            {/* <div className='m-2 p-2 cursor-pointer transition duration-200 hover:scale-105'>
+                <img src="" alt="" />
+                <Link to="/update">Update</Link>
+                </div> */}
+            <div className='m-2 p-2 cursor-pointer transition duration-200 hover:scale-105 hover:text-red-500 flex flex-row  items-center space-x-2 sm:hiden'>
+                {showword && <img src={add} alt="" className='w-[20px] h-[20px]' onClick={()=>{navigate("/create")}}/>}
+                <Link to="/create" className='flex sm:hidden'>Create</Link>
+            </div>
+            <div className='m-2 p-2 cursor-pointer transition duration-200 hover:scale-105  hover:text-red-500 flex flex-row  items-center space-x-2'>
+                {showword &&<img src={employeedetail} alt="" className='w-[20px] h-[20px] bg-white rounded-[23px]' onClick={()=>{navigate("/")}}/>}
+                <Link to="/" className='flex sm:hidden'>Employee Detail</Link>
+            </div>
+            {/* <div className='m-2 p-2 cursor-pointer transition duration-200 hover:scale-105'>
+                <img src="" alt="" />
+                <Link to="/delete">Delete</Link>
+            </div> */}
+        </div>
+        <div>
+        <div className='m-2 p-2 cursor-pointer transition duration-200 hover:scale-105  hover:text-red-500 flex flex-row  items-center space-x-2' onClick={()=>{change()}}>
+                 {!isAuth && showword && <img src={login} alt="" className='w-[20px] h-[20px] '/>}
+                {isAuth && showword && <img src={logout} alt="" className='w-[20px] h-[20px] '/>}
+
+                <button onClick={()=>{change()} } className='flex sm:hidden'>{isAuth?"logout":"login"}</button>
+        </div>
+        
+        {!isAuth && <div className='m-2 p-2 cursor-pointer transition duration-200 hover:scale-105  hover:text-red-500 flex flex-row  items-center space-x-2'>
+                {showword && <img src={signupi} alt="" onClick={()=>{navigate("/signup")}} className='w-[20px] h-[20px] bg-white rounded-[23px]'/>}
+                <Link to="/signup" className='flex sm:hidden'>Sign up</Link>
+        </div>}
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
